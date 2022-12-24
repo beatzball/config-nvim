41299deb50ed68a74261256004151353e9b21ec6
Beatzball <github@beatzball.com>
2022-12-23 16:48:14 -0800

    feat: beatzball plugins init

---
+++ init.lua
@@ -0,0 +1,1 @@
+require("beatzball.init")
---
+++ lua/beatzball/plugins/which-key.lua
@@ -0,0 +1,19 @@
+-- import gitsigns plugin safely
+local setup, wk = pcall(require, "which-key")
+if not setup then
+	return
+end
+
+-- configure/enable which-key
+local mappings = {
+	["<leader>"] = {
+		s = { name = "Split" },
+		t = { name = "Tab" },
+		e = { name = "Explorer" },
+		f = { name = "Find" },
+		g = { name = "Git" },
+		r = { name = "Restart" },
+	},
+}
+local opts = {}
+wk.register(mappings, opts)
